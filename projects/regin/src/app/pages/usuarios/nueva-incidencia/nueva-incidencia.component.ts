9005bba97c22aa690aa61388caaf6ad391dd4c80
Dani <daniel.bayarri@hospitalprovincial.es>
2024-09-06 15:39:00 +0200

    regin con usuario y logout

--- projects/regin/src/app/pages/usuarios/nueva-incidencia/nueva-incidencia.component.ts
+++ projects/regin/src/app/pages/usuarios/nueva-incidencia/nueva-incidencia.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { BreadcrumbComponent } from '../../../shared/components/breadcrumb/breadcrumb.component';
-import { CalendarComponent } from '../../../shared/components/calendar/calendar.component';
-import { InputComponent } from '../../../shared/ui/input/input.component';
-import { SelectComponent } from '../../../shared/ui/select/select.component';
-import { Grupo } from '../../../core/interfaces/grupo.interface';
+import { SelectComponent } from '../../../assets/ui/select/select.component';
+import { InputComponent } from '../../../assets/ui/input/input.component';
+import { CalendarComponent } from '../../../assets/components/calendar/calendar.component';
+import { BreadcrumbComponent } from '../../../assets/components/breadcrumb/breadcrumb.component';
 import { GrupoService } from '../../../core/services/grupo.service';
-
+import { GrupoInterface } from '../../../../../../shared/interfaces/grupo.interface';
 @Component({
   selector: 'app-nueva-incidencia',
   standalone: true,
@@ -21,7 +20,7 @@ import { GrupoService } from '../../../core/services/grupo.service';
   templateUrl: './nueva-incidencia.component.html',
 })
 export class NuevaIncidenciaComponent implements OnInit {
-  public gruposList$!: Observable<Grupo[]>;
+  public gruposList$!: Observable<GrupoInterface[]>;
 
   constructor(private grupoService: GrupoService) {}
 
